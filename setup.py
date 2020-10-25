3315802bca722fa69a5dbf3362935fca7c06b690
Gianluca Scarpellini <gianlucascarpellini@MacBook-Pro-di-Gianluca.local>
2020-10-25 22:56:04 +0100

    Fix for setup

--- setup.py
+++ setup.py
@@ -9,22 +9,22 @@ with open("README.md", "r") as fh:
         open("deep_toggl/__init__.py").read(),
         re.MULTILINE,
     ).group(1)
-
 setuptools.setup(
     name="deep_toggl",
     version=version,
     author="gianscarpe",
     author_email="me@scarpellini.dev",
+    long_description_content_type="text/markdown",
     description="Simple tool for toggl summary",
     long_description=long_description,
     url="https://github.com/gianscarpe/sampleproject",
-    entry_points={'console_scripts': ['deep_toggl = deep_toggl:main']},
-    install_requires=['pendulum', 'togglCli', 'calmap'],
+    entry_points={"console_scripts": ["deep_toggl = deep_toggl:main"]},
+    install_requires=["pendulum", "togglCli", "calmap"],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
